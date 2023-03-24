60677caa62d6d2dd9ddabeed333603576ad9ef12
Olivier Delmotte <olivier@odelmotte.fr>
2023-03-24 23:28:10 +0100

    update

---
+++ src/h18.Azdo.Library.Editor.Logic/LibraryEntry.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace h18.Azdo.Library.Editor.Logic
+{
+    public class LibraryEntry : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public LibraryEntry(string value)
+        {
+            _Value = value;
+        }
+
+        private string _Value;
+        public string Value
+        {
+            get { return _Value; }
+            set
+            {
+                if (string.Equals(_Value, value, System.StringComparison.InvariantCulture))
+                {
+                    _Value = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs(nameof(Value)));
+                    }
+                }
+            }
+        }
+    }
+}
